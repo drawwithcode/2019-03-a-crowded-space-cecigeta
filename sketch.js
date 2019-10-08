ae5b5d870cf7ddc7ab92b03ba8cf9765ebbef0ad
Cecilia <cecilia.gaetarelli@gmail.com>
2019-10-08 11:14:46 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,28 @@
 function preload(){
   // put preload code here
 }
+var img;
+var coppiaPrima;
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  img = loadImage("coppia1.png");
+
+  coppiaPrima = new People (width/2, height/2); 
+
+
 }
 
 function draw() {
   // put drawing code here
+}
+
+function People(_x,_y) {
+
+  this.x = _x;
+  this.y = _y;
+
+  this.display = function() {
+    image(img, this.x, this.y);
+  }
 }
